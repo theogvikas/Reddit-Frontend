3dd48beeab45a37360a4a2ca08a187e2e6b1d68c
theogvikas <kumaarrvikaas@gmail.com>
2026-08-24 08:15:44 +0530

    Remove duplicate fetchUserProfile declaration

--- src/api/userService.js
+++ src/api/userService.js
@@ -26,17 +26,6 @@ export const fetchUserProfile = async (username) => {
     return { isSuccess: false, errMsg: "Something went wrong!" };
   }
 };
-
-// Fetch a user's public profile
-export const fetchUserProfile = async (username) => {
-  try {
-    const response = await axiosInstance.get(`/auth/profile/${username}`);
-    return response.data;
-  } catch (error) {
-    return { isSuccess: false, errMsg: "Something went wrong!" };
-  }
-};
-
 // Handle follow/unfollow toggle
 export const followUser = async (userId) => {
   try {
